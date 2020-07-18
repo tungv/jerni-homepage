b834d15c757402a9ee6148b280e255687cfd0a60
Tung <me@tungv.com>
2020-07-18 18:21:24 +0700

    doc: wip home page

--- www/src/components/CodeFile.js
+++ www/src/components/CodeFile.js
@@ -5,25 +5,32 @@ export default function CodeFile(props) {
   const { fileName, code } = props;
 
   return (
-    <div>
+    <div className="rounded-lg overflow-hidden m-2 shadow-lg">
       <header>
-        <h6>
+        <h6 className="bg-black text-white py-1 px-3 border-b-2 border-green-300 flex items-center">
+          <span className="text-gray-300 font-mono text-sm mr-1">file: </span>
           <code>{fileName}</code>
         </h6>
-        <Highlight {...defaultProps} theme={theme} code={code} language="jsx">
-          {({ className, style, tokens, getLineProps, getTokenProps }) => (
-            <pre className={className} style={style}>
-              {tokens.map((line, i) => (
-                <div {...getLineProps({ line, key: i })}>
-                  {line.map((token, key) => (
-                    <span {...getTokenProps({ token, key })} />
-                  ))}
-                </div>
-              ))}
-            </pre>
-          )}
-        </Highlight>
       </header>
+
+      <Highlight
+        {...defaultProps}
+        theme={theme}
+        code={code.trim()}
+        language="jsx"
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style} className="p-2">
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
     </div>
   );
 }
--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -5,12 +5,7 @@ import CodeFile from "../components/CodeFile";
 
 export default function JerniHomePage() {
   return (
-    <main>
-      <h1>
-        <header>jerni</header>
-        <span>a framework to build data-driven product from the ground up</span>
-      </h1>
-
+    <main className="m-12">
       <nav>
         <ul>
           <li>
@@ -35,32 +30,112 @@ export default function JerniHomePage() {
           </li>
         </ul>
       </nav>
+      <div className="grid grid-cols-1 gap-6">
+        <h1 className="mb-4">
+          <header className="text-5xl font-mono">jerni</header>
+          <span className="text-lg font-serif">
+            a framework to build data-driven product from the ground up
+          </span>
+        </h1>
 
-      <section>
-        <header>
-          <h3>Event-driven + Functional Projection</h3>
-        </header>
+        <section className="my-6">
+          <header className="max-w-4xl m-auto text-center">
+            <h3 className="text-5xl">Event-driven + Functional Projection</h3>
+          </header>
 
-        <div>
-          <EventFlow
-            app={<CodeFile fileName="app.js" code={EVENT_DRIVEN_APP_JS} />}
-            job={
-              <CodeFile
-                fileName="models/accounts.js"
-                code={EVENT_DRIVEN_JOB_JS}
-              />
-            }
-          />
-        </div>
-      </section>
+          <p className="max-w-4xl m-auto text-xl font-serif p-2">
+            jerni is built primarily on the concepts of{" "}
+            <a
+              href="https://martinfowler.com/eaaDev/EventSourcing.html"
+              target="_blank"
+            >
+              Event Sourcing
+            </a>{" "}
+            and{" "}
+            <a href="https://martinfowler.com/bliki/CQRS.html" target="_blank">
+              CQRS
+            </a>
+            . It also embraces the functional programming techniques to simplify
+            state-changing operations without sacrifice flexibility.
+          </p>
 
-      <section>
-        <header>
-          <h3>Strictly Ordered + Exactly Once Delivery</h3>
-        </header>
+          <p className="max-w-4xl m-auto text-lg font-serif p-1 px-6">
+            <strong>Event-Driven:</strong> You describe your business logic
+            using a sequence of events. Those events are defined with the
+            vocabulary of business users instead of technical terms to ensure
+            the system is resilient to underlaying technological changes.
+          </p>
 
-        <div></div>
-      </section>
+          <p className="max-w-4xl m-auto text-lg font-serif p-1 px-6">
+            <strong>Functional Projections:</strong> Events are translated into
+            technical terms in order to interact with storage layers. These
+            translations are done in a functional manner. This guarantees you
+            can always reproduce the state of your data at any point in time.
+          </p>
+
+          <div>
+            <EventFlow
+              app={
+                <CodeFile
+                  fileName="controllers/accounts.js"
+                  code={EVENT_DRIVEN_APP_JS}
+                />
+              }
+              job={
+                <CodeFile
+                  fileName="models/accounts.js"
+                  code={EVENT_DRIVEN_JOB_JS}
+                />
+              }
+            />
+          </div>
+        </section>
+
+        <section className="my-6">
+          <header className="max-w-4xl m-auto text-center">
+            <h3 className="text-5xl">
+              Strict Order &amp; Exactly Once Delivery
+            </h3>
+          </header>
+
+          <p className="max-w-4xl m-auto text-xl font-serif p-2">
+            jerni takes a few trade-offs in term of maximizing speed in order to
+            ensure 2 important assumptions in data flow. Pessimistic locking is
+            a thing in a past.
+          </p>
+
+          <p className="max-w-4xl m-auto text-lg font-serif p-1 px-6">
+            <strong>Strict Order:</strong> after being committed, each event
+            will receive a globally monotonic increasing numerical ID. All
+            processing is then strictly done following that order{" "}
+            <b>No Matter What&trade;</b>
+          </p>
+
+          <p className="max-w-4xl m-auto text-lg font-serif p-1 px-6">
+            <strong>Exactly Once Delivery:</strong> Never have to worry about
+            missing or duplicate events. Events are not only come in order but
+            also come once and only once. However, right when you need to, they
+            are there for you to re-run just like the first time.
+          </p>
+
+          <div>
+            <EventFlow
+              app={
+                <CodeFile
+                  fileName="controllers/accounts.js"
+                  code={STRICT_ORDER_APP_JS}
+                />
+              }
+              job={
+                <CodeFile
+                  fileName="models/accounts.js"
+                  code={STRICT_ORDER_JOB_JS}
+                />
+              }
+            />
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
@@ -69,40 +144,99 @@ function EventFlow(props) {
   const { app, job } = props;
 
   return (
-    <div>
-      <div>{app}</div>
-      <div>queue</div>
-      <div>{job}</div>
+    <div className="flex flex-row items-center max-w-full">
+      <div className="flex-1">{app}</div>
+      <div className="">
+        <img
+          width={300}
+          src="/events-flow.png"
+          alt="events are sent through an events queue"
+        />
+      </div>
+      <div className="flex-1">{job}</div>
     </div>
   );
 }
 
 const EVENT_DRIVEN_APP_JS = `
-  import journey from './journey.js';
-  
-  // to make a change, commit an event
-  await journey.commit({
-    type: "ACCOUNT_CREATED",
-    payload: { id: "8aud23jm", account_name: "Alice" }
-  });
+import journey from './journey.js';
+
+// to make a change, commit an event
+await journey.commit({
+  type: "ACCOUNT_CREATED",
+  payload: { id: "8aud23jm", account_name: "Alice" }
+});
 `;
 
 const EVENT_DRIVEN_JOB_JS = `
-  import { Model } from '@jerni/store-mongo';
-  import mapEvents from 'jerni/lib/mapEvents';
+import { Model } from '@jerni/store-mongo';
+import mapEvents from 'jerni/lib/mapEvents';
 
-  export default new Model({
-    name: 'accounts',
-    transform: handleEvents({
-      ACCOUNT_CREATED(event) {
-        return {
-          insertOne: {
-            id: event.payload.id,
-            name: event.payload.account_name,
-            balance: 0
-          }
+export default new Model({
+  name: 'accounts',
+  transform: handleEvents({
+    ACCOUNT_CREATED(event) {
+      return {
+        insertOne: {
+          id: event.payload.id,
+          name: event.payload.account_name,
+          balance: 0
         }
       }
-    })
+    }
   })
+})
+`;
+
+const STRICT_ORDER_APP_JS = `
+import journey from './journey.js';
+
+// committed events are assigned with an ID
+const event = await journey.commit({
+  type: "MONEY_SENT",
+  payload: {
+    src: "8aud23jm",
+    dest: "aot42lfe",
+    amount: 100,
+    src_currency: "USD",
+    dest_currency: "EUR",
+    rate: 0.88,
+  }
+});
+`;
+
+const STRICT_ORDER_JOB_JS = `
+export default new Model({
+  name: 'accounts',
+  transform: handleEvents({
+    ACCOUNT_CREATED(event) { /* ... */ },
+    MONEY_SENT(event) {
+      const {
+        src,
+        dest,
+        amount,
+        rate,
+      } = event.payload;
+      
+      return [
+        {
+          updateOne: {
+            where: { id: src },
+            changes: {
+              $inc: { balance: amount * -1 }
+            }
+          }
+        },
+        {
+          updateOne: {
+            where: { id: dest },
+            changes: {
+              $inc: { balance: amount * rate }
+            }
+          }
+        },
+      ]
+    }
+  })
+})
 `;
