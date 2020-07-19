9c14535981704a0e775c31a8e7b562ddcd9ca8e5
Tung <me@tungv.com>
2020-07-19 14:59:06 +0700

    doc: commit event

--- www/src/components/CodeFile.js
+++ www/src/components/CodeFile.js
@@ -1,8 +1,16 @@
 import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/nightOwl";
+import { Fragment } from "react";
 
 export default function CodeFile(props) {
-  const { fileName, code, children, language = "jsx" } = props;
+  const {
+    fileName,
+    code,
+    children,
+    language = "jsx",
+    result,
+    outputLanguage,
+  } = props;
 
   return (
     <div className="rounded-lg overflow-hidden shadow-lg text-xs sm:text-sm md:text-md">
@@ -15,28 +23,79 @@ export default function CodeFile(props) {
         </header>
       )}
 
-      <Highlight
-        {...defaultProps}
-        theme={theme}
-        code={(code || children).trim()}
-        language={language}
-      >
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre
-            className={className}
-            style={style}
-            className="p-2 overflow-auto"
-          >
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
+      {language === "text" && (
+        <pre
+          style={{
+            background: theme.plain.backgroundColor,
+            color: theme.plain.color,
+          }}
+          className="p-2 overflow-auto"
+        >
+          {React.Children.toArray(children).map((line, index) => {
+            return (
+              <div
+                className="token-line mr-1"
+                key={index}
+                style={{ color: theme.plain.color }}
+              >
+                <span className="token plain">{line}</span>
               </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
+            );
+          })}
+        </pre>
+      )}
+
+      {language !== "text" && (
+        <Highlight
+          {...defaultProps}
+          theme={theme}
+          code={(code || children).trim()}
+          language={language}
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre
+              className={className}
+              style={style}
+              className="p-2 overflow-auto"
+            >
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
+      )}
+      {result && (
+        <Fragment>
+          <h3 className="bg-green-500 text-white px-2 py-1">Output</h3>
+          <Highlight
+            {...defaultProps}
+            theme={theme}
+            code={result.trim()}
+            language={outputLanguage || language}
+          >
+            {({ className, style, tokens, getLineProps, getTokenProps }) => (
+              <pre
+                className={className}
+                style={style}
+                className="p-2 overflow-auto"
+              >
+                {tokens.map((line, i) => (
+                  <div {...getLineProps({ line, key: i })}>
+                    {line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                ))}
+              </pre>
+            )}
+          </Highlight>
+        </Fragment>
+      )}
     </div>
   );
 }
--- www/src/pages/tutorials/getting-started.js
+++ www/src/pages/tutorials/getting-started.js
@@ -157,6 +157,295 @@ module.exports = new Model({
             Views in SQL, they are derived from the sequence of events.
           </p>
         </div>
+
+        <p>
+          To verify this step, you can run this bash script in your terminal at
+          the project root directory, given you have your local mongodb server
+          listening on its default port
+        </p>
+
+        <CodeFile
+          language="bash"
+          outputLanguage="text"
+          code={`MONGODB_URL=mongodb://localhost:27017 MONGODB_DBNAME=banks ${
+            useYarn
+              ? "yarn jerni-dev start ./journey"
+              : "npx jerni-dev start ./journey"
+          }`}
+          result={`[ cli ] info: jerni-dev.start({ version: '1.0.0' })
+[ cli ] info:  * source file: /path/to/your/my-first-jerni-project/banks/journey
+[ cli ] info:  * options { http: 6181, verbose: false, dataPath: 'jerni.db' }
+[ cli ] info: checking integrity of data file since last start
+[ cli ] info: heq-server is listening on port 6181
+[ cli ] info: writing lockfile to .jerni-dev
+[ cli ] info: clean start new journey
+[ cli ] info: worker ready
+[jerni] info: start receiving data
+jerni-dev>`}
+        ></CodeFile>
+
+        <p>
+          You can use <kbd>Ctrl-C</kbd> to terminate this process
+        </p>
+      </section>
+
+      <section className="w-full max-w-4xl m-auto p-2 grid grid-cols-1 gap-2">
+        <header>
+          <h3 className="text-2xl">Commit Event</h3>
+        </header>
+
+        <p>
+          First, we need to create an <code>express</code> app to listen on port
+          3000.
+        </p>
+
+        <CodeFile language="bash">
+          {useYarn ? `yarn add express` : `npm i express`}
+        </CodeFile>
+
+        <p>
+          Then create a <code>server.js</code> file at project root directory,
+          with the following content:
+        </p>
+        <CodeFile fileName="server.js" language="js">
+          {`
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/api/accounts", (req, res) => {
+  res.send([]);
+});
+
+app.post("/api/accounts", express.json(), async (req, res) => {
+  const { name, currency } = req.body;
+  const id = Math.trunc(Math.random() * 1e11) + 1e12;
+
+  res.send({ name, balance: { [currency]: 0 }, id });
+});
+
+
+app.listen(port, () =>
+  console.log(\`Example app listening at http://localhost:$\{port\}\`),
+);
+`}
+        </CodeFile>
+
+        <p>And finally start the app by running this bash script in terminal</p>
+
+        <CodeFile
+          language="bash"
+          code={`node server.js`}
+          result={`Example app listening at http://localhost:3000`}
+        ></CodeFile>
+
+        <p>
+          Leaving this process running, in another terminal, let run a quick
+          test to see if it works
+        </p>
+
+        <p>GET</p>
+        <CodeFile
+          language="bash"
+          outputLanguage="json"
+          code={`curl localhost:3000/api/accounts`}
+          result={`[]`}
+        ></CodeFile>
+
+        <p>POST</p>
+
+        <CodeFile
+          language="bash"
+          outputLanguage="json"
+          code={`
+curl -XPOST localhost:3000/api/accounts \\
+     -H"content-type: application/json" \\
+     -d '{"name":"test", "currency":"USD"}'`}
+          result={`{"name":"test","balance":{"USD":0},"id":1014151097134}`}
+        ></CodeFile>
+
+        <p>
+          Nothing fancy yet, now let's initialize <code>jerni</code> to our
+          server.
+        </p>
+
+        <CodeFile language="js">
+          {`
+/* ... */
+app.post("/api/accounts", express.json(), async (req, res) => {
+  const { name, currency } = req.body;
+  const id = Math.trunc(Math.random() * 1e11) + 1e12;
+
+  await journey.commit({
+    type: "ACCOUNT_CREATED",
+    payload: { id, account_name: name, currency },
+  });
+
+  res.send({ name, balance: { [currency]: 0 }, id });
+});
+/* ... */
+`}
+        </CodeFile>
+
+        <p>
+          Restart express server, and try to <code>POST /api/accounts</code>{" "}
+          again
+        </p>
+
+        <CodeFile
+          language="bash"
+          code={`
+curl -XPOST localhost:3000/api/accounts \\
+     -H"content-type: application/json" \\
+     -d '{"name":"test", "currency":"USD"}'`}
+          result={`curl: (52) Empty reply from server`}
+        ></CodeFile>
+
+        <p>You will see a new line of message</p>
+
+        <CodeFile language="text">
+          <span>journey is ready!</span>
+          <span>Example app listening at http://localhost:3000</span>
+          <span>
+            <span className="rounded bg-red-700 text-white text-bold">
+              {" "}
+              jerni-dev{" "}
+            </span>{" "}
+            invalid jerni server provided, received: .
+          </span>
+          <br></br>
+          <span>
+            {"  "}
+            <span className="text-gray-500">1)</span> if you're in{" "}
+            <span className="text-green-500 text-bold">development</span> mode,
+            make sure you ran jerni-dev.
+          </span>
+          <span>
+            {"  "}
+            <span className="text-gray-500">2)</span> if you're in{" "}
+            <span className="text-green-500 text-bold">production</span> mode,
+            please set{" "}
+            <span className="text-bold text-white">NODE_ENV=production</span>{" "}
+            before starting your app
+          </span>
+        </CodeFile>
+
+        <p>
+          jerni-dev It prompts you to run <code>jerni-dev</code>. Open another
+          terminal (this is your last terminal to open) and run:
+        </p>
+
+        <CodeFile
+          language="bash"
+          outputLanguage="text"
+          code={`MONGODB_URL=mongodb://localhost:27017 MONGODB_DBNAME=banks ${
+            useYarn
+              ? "yarn jerni-dev start ./journey"
+              : "npx jerni-dev start ./journey"
+          }`}
+          result={`[ cli ] info: jerni-dev.start({ version: '1.0.0' })
+[ cli ] info:  * source file: /path/to/your/my-first-jerni-project/banks/journey
+[ cli ] info:  * options { http: 6181, verbose: false, dataPath: 'jerni.db' }
+[ cli ] info: checking integrity of data file since last start
+[ cli ] info: heq-server is listening on port 6181
+[ cli ] info: writing lockfile to .jerni-dev
+[ cli ] info: clean start new journey
+[ cli ] info: worker ready
+[jerni] info: start receiving data
+jerni-dev>`}
+        ></CodeFile>
+
+        <p>
+          Restart express server, and try to <code>POST /api/accounts</code>{" "}
+          again
+        </p>
+
+        <CodeFile
+          language="bash"
+          outputLanguage="json"
+          code={`
+curl -XPOST localhost:3000/api/accounts \\
+     -H"content-type: application/json" \\
+     -d '{"name":"test", "currency":"USD"}'`}
+          result={`{"name":"test","balance":{"USD":0},"id":1024340236565}`}
+        ></CodeFile>
+
+        <p>
+          Notice the messages from <code>express</code> server terminal:
+        </p>
+
+        <CodeFile language="text">
+          <span>
+            <span className="text-white bg-green-700 rounded"> jerni-dev </span>{" "}
+            running in development mode
+          </span>
+          <br />
+          <span>versions:</span>
+          <span>
+            {"  "}jerni:{"     "}
+            <span className="text-white text-bold">1.0.0</span>
+          </span>
+          <span>
+            {"  "}jerni-dev: <span className="text-white text-bold">1.0.0</span>
+          </span>
+          <br />
+          <span>heq-server:</span>
+          <span>
+            {"  "}original URL:{" "}
+            <span className="italic text-gray-500">undefined</span> (not used in
+            development mode)
+          </span>
+          <span>
+            {"  "}dev server URL:{" "}
+            <span className="itatic text-green-500 underline">
+              http://localhost:6181
+            </span>
+          </span>
+          <br />
+          <span>stores:</span>
+          <span>
+            {"  "}-{" "}
+            <span className="itatic text-green-500 underline">
+              mongodb://localhost:27017
+            </span>
+          </span>
+          <br />
+          <br />
+          <span>
+            <span className="text-white bg-green-700 rounded"> jerni-dev </span>{" "}
+            event{" "}
+            <span className="text-white text-bold">
+              #1 [type=ACCOUNT_CREATED]
+            </span>{" "}
+            has been committed to dev server at{" "}
+            <span className="itatic text-green-500 underline">
+              http://localhost:6181
+            </span>
+          </span>
+        </CodeFile>
+
+        <p>
+          You have successfully committed your first jerni event. To double
+          check, open the file <code>jerni.db</code> that <code>jerni-dev</code>{" "}
+          has created. You will see this:
+        </p>
+
+        <CodeFile language="text" fileName="jerni.db">
+          {`### BEGIN checksum: e07846fb1760381ec374b65a9ccc1605 ###
+{"type":"ACCOUNT_CREATED","payload":{"id":1024340236565,"account_name":"test","currency":"USD"},"meta":{"occurred_at":1595144407458,"client":"jerni-banks-demo","clientVersion":"1.0.0"}}`}
+        </CodeFile>
+
+        <div className="p-4 border-l-4 border-green-500 bg-green-100 grid grid-cols-1 gap-4">
+          <p>
+            <b>Note:</b> whether or not to include this file to source control
+            is up to you.
+          </p>
+          <p>
+            We recommend to commit this file to source control. However, review
+            the diff before you commit to make sure it contains only useful
+            seeding data for the next developer to begin with.
+          </p>
+        </div>
       </section>
     </HomeLayout>
   );
