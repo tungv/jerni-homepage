19992bd0a21ab1a3a40b17507938481cb1fb2be0
Tung <me@tungv.com>
2020-07-23 23:50:26 +0700

    feat: support extends Custom types

--- www/src/components/TypeDetail.js
+++ www/src/components/TypeDetail.js
@@ -4,9 +4,20 @@ import CodeFile from "./CodeFile";
 import DataType from "./DataType";
 import HomeLayout from "./HomeLayout";
 import Link from "next/link";
+import linkToType from "../linkToTypes";
+import getTypeProperties from "../getTypeProperties";
 
 export default function TypeDetail(props) {
   const { pkg, type } = props;
+
+  const inherits = Array.isArray(type.extends)
+    ? getTypeProperties(...type.extends)
+        .map((t) => ({ ...t, from: type.extends }))
+        .filter((t) =>
+          type.properties.some((ownProp) => ownProp.name !== t.name),
+        )
+    : [];
+
   return (
     <HomeLayout title={`jerni - type definition - ${type.name}`}>
       <header>
@@ -27,7 +38,13 @@ export default function TypeDetail(props) {
             <tr>
               <td className="text-gray-500 px-2 py-1 text-right">Extends:</td>
               <td className="font-mono">
-                <DataType type={type.extends}></DataType>
+                {Array.isArray(type.extends) ? (
+                  <Link {...linkToType(...type.extends)}>
+                    <a>{type.extends.join("::")}</a>
+                  </Link>
+                ) : (
+                  <DataType type={type.extends}></DataType>
+                )}
               </td>
             </tr>
 
@@ -43,11 +60,41 @@ export default function TypeDetail(props) {
         </table>
       </header>
 
-      {type.properties && (
+      {(type.properties || inherits.length > 0) && (
         <section className="p-2 grid grid-cols-1 gap-4 max-w-6xl">
           <header>
             <h4 className="font-bold text-lg">Properties</h4>
           </header>
+          {inherits.map((prop) => (
+            <section key={prop.name} className="border rounded shadow m-2">
+              <div className="px-4 text-sm bg-teal-500 text-white">
+                inherited from{" "}
+                <Link {...linkToType(...type.extends)}>
+                  <a className="font-mono text-white">
+                    {type.extends.join("::")}
+                  </a>
+                </Link>
+              </div>
+              <header className="bg-gray-200 px-4 py-2">
+                <div className="flex flex-row items-center flex-wrap">
+                  <h5 className="font-bold">{prop.name}</h5>
+                  <code className="mr-4">
+                    : <DataType {...prop}></DataType>
+                  </code>
+                  <p>{prop.description || <em>--</em>}</p>
+                </div>
+              </header>
+
+              {prop.examples && (
+                <div className="p-4 grid grid-cols-1 gap-4">
+                  <header>
+                    <h6 className="font-bold">Examples</h6>
+                  </header>
+                  <Examples examples={prop.examples} />
+                </div>
+              )}
+            </section>
+          ))}
 
           {type.properties.map((prop) => (
             <section key={prop.name} className="border rounded shadow m-2">
@@ -66,33 +113,7 @@ export default function TypeDetail(props) {
                   <header>
                     <h6 className="font-bold">Examples</h6>
                   </header>
-                  {prop.examples.map((example, index) =>
-                    example.code ? (
-                      <div key={index}>
-                        <CodeFile {...example}></CodeFile>
-                      </div>
-                    ) : example.ref ? (
-                      <span key={index}>
-                        see{" "}
-                        <Link
-                          href={{
-                            pathname: "/references/types/[...ns]",
-                            query: { ns: example.ref.join("/") },
-                          }}
-                          as={`/references/types/${example.ref.join("/")}`}
-                        >
-                          <a>
-                            <code>{example.ref[1]}</code>
-                          </a>
-                        </Link>{" "}
-                        from package: <code>{example.ref[0]}</code>
-                      </span>
-                    ) : (
-                      <code key={index}>
-                        <pre>{JSON.stringify(example, null, 2)}</pre>
-                      </code>
-                    ),
-                  )}
+                  <Examples examples={prop.examples} />
                 </div>
               )}
             </section>
@@ -100,5 +121,35 @@ export default function TypeDetail(props) {
         </section>
       )}
     </HomeLayout>
+  );
+}
+
+function Examples(props) {
+  return props.examples.map((example, index) =>
+    example.code ? (
+      <div key={index}>
+        <CodeFile {...example}></CodeFile>
+      </div>
+    ) : example.ref ? (
+      <span key={index}>
+        see{" "}
+        <Link
+          href={{
+            pathname: "/references/types/[...ns]",
+            query: { ns: example.ref.join("/") },
+          }}
+          as={`/references/types/${example.ref.join("/")}`}
+        >
+          <a>
+            <code>{example.ref[1]}</code>
+          </a>
+        </Link>{" "}
+        from package: <code>{example.ref[0]}</code>
+      </span>
+    ) : (
+      <code key={index}>
+        <pre>{JSON.stringify(example, null, 2)}</pre>
+      </code>
+    ),
   );
 }
